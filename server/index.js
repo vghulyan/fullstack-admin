6271dd84842cc40e4739072f3342dd6cb2e97352
vghulyan <vghulyan@yahoo.com>
2022-12-17 14:20:55 +0000

    initial commit

---
+++ server/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import bodyParser from "body-parser"; // body-parser - parsing our data coming in
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet"; // helmet - protecting api
+import morgan from "morgan"; // morgan - logging our api calls
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// data imports
+import User from "./models/User.js";
+import Product from "./models/Product.js";
+import ProductStat from "./models/ProductStat.js";
+import Transaction from "./models/Transaction.js";
+import OverallStat from "./models/OverallStat.js";
+import AffiliateStat from "./models/AffiliateStat.js";
+import {
+  dataUser,
+  dataProduct,
+  dataProductStat,
+  dataTransaction,
+  dataOverallStat,
+  dataAffiliateStat,
+} from "./data/index.js";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes); // Client Facing: [Products, Customers, Transactions, Geography]
+app.use("/general", generalRoutes); // getting users & dashboard
+app.use("/management", managementRoutes); // Management: [Admin, Performance]
+app.use("/sales", salesRoutes); // Sales: [Overview, Daily, Monthly, Breakdown]
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.set("strictQuery", true); // to keep the Mongoose happy
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ONLY ADD DATA ONE TIME */
+    // AffiliateStat.insertMany(dataAffiliateStat);
+    // OverallStat.insertMany(dataOverallStat);
+    // Product.insertMany(dataProduct);
+    // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
+    // User.insertMany(dataUser);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
